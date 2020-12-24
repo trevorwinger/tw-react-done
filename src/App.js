a546e0ab2e78bf22ce490843f723efa9e3dc641c
Trevor <trevor.winger@artunlimitedusa.com>
2020-12-24 11:07:19 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './tailwind.output.css';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <div>
+      <Body header_text={'trev tech'} main_head={'tech but in the future'}/>
+    </div>
+  );
+}
+
+export default App;
